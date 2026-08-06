9fd2749f19fc6e770b6d4e85d32d26404d96c2b0
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2026-08-06 12:59:08 +0600

    about

--- components/About.tsx
+++ components/About.tsx
@@ -1,26 +1,29 @@
-import { ArrowDownRight } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 import React from "react";
 
 type Props = {};
 
 const About: React.FC<Props> = () => {
+  const aboutText: string =
+    "<span> Welcome to Vorklye — We're a </span> Shopify design and development <span>studio, dedicated to bringing your brand's essence to life online. From custom themes to seamless user experiences, every project is a bespoke journey — we collaborate closely with you to build something that truly stands out. </span>";
   return (
-    <div className="border-b border-neutral-900 py-42.5 text-center">
-      <div className="container m-auto">
-        <p className="max-w-276 m-auto text-center justify-start text-gray-200 text-3xl font-medium leading-12.5">
-          Welcome to Vorklye — We&apos;re a Shopify design and development
-          studio, dedicated to bringing your brand&apos;s essence to life
-          online. From custom themes to seamless user experiences, every project
-          is a bespoke journey — we collaborate closely with you to build
-          something that truly stands out.
-        </p>
-
-        <a href="#" className="btn-underline mt-17.5 ">
-          <span>More about us</span>
-          <ArrowDownRight />
-        </a>
+    aboutText && (
+      <div className="border-b border-neutral-900 py-42.5 text-center">
+        <div className="container m-auto">
+          <p
+            className="text-center section-title"
+            dangerouslySetInnerHTML={{ __html: aboutText }}
+          ></p>
+          <a
+            href="#"
+            className="btn-underline mt-17.5 text-2xl font-semibold pb-2.5"
+          >
+            <span>More about us</span>
+            <ArrowUpRight size={20} />
+          </a>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
